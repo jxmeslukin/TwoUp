764c797b2b595c67bd3dfafb4889f6ea7c3642f6
jxmeslukin <jimmy.lukin@gmail.com>
2021-03-02 11:25:44 +1100

    v3

--- twoUpScript.js
+++ twoUpScript.js
@@ -3,31 +3,36 @@ https://www.ramint.gov.au/sites/default/files/2019/Dollar_Discovery/1-standard-k
 https://onlinecoin.club/images/coins/Australia/368e9900-ed2d-414f-9dc2-cc8bdb5e0a15.jpg
 */
 
-var wins = 0
-var losses = 0 
-var odds = 0
-var fiveOdds = 0
-var numFiveOdds = 0
-var bet = null
-var coinOne = null
-var coinTwo = null
-var numThrows = null
-var i = 0
-var coinChoice = null
-var betAmount = null
-var myList = document.getElementById('listBox')
-var newItem = ""
+
 
 function game() {
-    
 
-    for(i=0; i>totalValues; i++) {
-        coinOne = Math.floor(Math.random() * 2)
-        coinTwo = Math.floor(Math.random() * 2)
-        umThrows = document.getElementById("numRolls").value;
+    var wins = 0
+    var losses = 0 
+    var odds = 0
+    var fiveOdds = 0
+    var numFiveOdds = 0
+    var bet = document.getElementById('betAmount').value;
+    var numThrows = document.getElementById('numRolls').value;
+    var i = 0
+    var coinChoice = null
+    var betAmount = null
+    var myList = document.getElementById('listBox')
+    var newItem = ""
+    console.log(numThrows);
+    clearListBox(myList);
+
+    for(i=0; i < numThrows; i++) {
+        var coinOne = Math.floor(Math.random() * 2)
+        var coinTwo = Math.floor(Math.random() * 2)
+        console.log(coinOne);
+        numThrows = document.getElementById("numRolls").value;
         if(numThrows.length === 0) {
             return(alert("Enter an amount of rolls."));
     }
+        if(bet.length === 0) {
+            return(alert("Enter a bet."));
+        }
         if(document.getElementById('headChoice').checked) {
             coinChoice = "H"
     }
@@ -59,15 +64,37 @@ function game() {
             case "TTT":
                 odds = 0
                 wins = wins + 1
-                newItem = "Throw " + (i+1) + " " + totalValues + " - Win";
+                bet = bet * 2;
+                newItem = "Throw " + (i+1) + ": " + totalValues + " - Win - Bet: " + bet;
                 myList.value = myList.value + newItem + "\n";
+                
                 break;
             case "HTT":
             case "THH":
                 odds = 0
                 losses = losses + 1
-                newItem = "Throw " + (i+1)
+                bet = bet / 2;
+                newItem = "Throw " + (i+1) + ": " + totalValues + " - Lose - Bet: " + bet;
+                myList.value = myList.value + newItem + "\n";
+                break;
+            default:
+                odds = odds + 1 
+                if(odds === 5) {
+                    bet = bet - (bet / 2);
+                    newItem = "Throw " + (i+1) + ": " + totalValues + " - Lose (Five Odds) - Bet: " + bet;
+                    myList.value = myList.value + newItem + "\n";
+                    fiveOdds = fiveOdds + 1 
+                    odds = 0
+                } else {
+                    newItem = "Throw " + (i+1) + ": " + totalValues + " - Odds - Bet: " + bet;
+                    myList.value = myList.value + newItem + "\n";
+                }
+                break;
          }
+         console.log(bet)
     }
 }
 
+function clearListBox(myList) {
+    myList.value = ""
+}
